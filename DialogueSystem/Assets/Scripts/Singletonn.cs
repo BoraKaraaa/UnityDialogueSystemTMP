50faadb3e5c9863bc47a82f6a91677bb9395c9a4
bora kara <kborakaraa@gmail.com>
2023-01-07 16:26:02 +0300

    Revert "singleton update"
    This reverts commit 12a9ef828b748683100e289c3cfe9ffdd0dc2437.

--- DialogueSystem/Assets/Scripts/Singletonn.cs
+++ DialogueSystem/Assets/Scripts/Singletonn.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 
 public abstract class Singletonn<T> : MonoBehaviour
-    where T : MonoBehaviour
+    where T : Component
 {
     private static T _instance;
-    public static T Instance 
-    {
-        get 
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<T>();
-                if (_instance == null) 
-                {
-                    Debug.Log( $"Singleton Object {typeof(T).FullName} can not found in scene");
+    public static T Instance {
+        get {
+            if (_instance == null) {
+                var objs = FindObjectsOfType (typeof(T)) as T[];
+                if (objs.Length > 0)
+                    _instance = objs[0];
+                if (objs.Length > 1) {
+                    Debug.LogError ("There is more than one " + typeof(T).Name + " in the scene.");
+                }
+                if (_instance == null) {
+                    GameObject obj = new GameObject ();
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    _instance = obj.AddComponent<T> ();
                 }
             }
             return _instance;
@@ -22,20 +25,17 @@ public abstract class Singletonn<T> : MonoBehaviour
 }
 
 public abstract class SingletonnPersistent<T> : MonoBehaviour
-    where T : MonoBehaviour
+    where T : Component
 {
     public static T Instance { get; private set; }
     
     public virtual void Awake ()
     {
-        if (Instance == null) 
-        {
+        if (Instance == null) {
             Instance = this as T;
-            DontDestroyOnLoad(this);
-        } 
-        else 
-        {
-            Destroy(this.gameObject);
+            DontDestroyOnLoad (this);
+        } else {
+            Destroy (gameObject);
         }
     }
 }
